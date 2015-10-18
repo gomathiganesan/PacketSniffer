9ffe3a050cb7f848bb5788eaa7be5f34a89f49ee
gomathiganesan <gomathiganesantvl@gmail.com>
2015-10-18 11:10:40 +0530

    Create pSniff.h

---
+++ pSniff.h
@@ -0,0 +1,28 @@
+     
+    #include <sys/types.h>
+    #include <sys/stat.h>
+    #include <unistd.h>
+    #include<stdio.h> 
+    #include<stdlib.h>    
+    #include<string.h>    
+    #include<netinet/ip_icmp.h>   
+    #include<netinet/udp.h>   
+    #include<netinet/tcp.h>   
+    #include<netinet/ip.h>    
+    #include<sys/socket.h>
+    #include<arpa/inet.h>
+    #define DO 60002
+    #define UNDO 60001
+    #define REDO 60000
+    typedef struct operation
+    {
+	    int flag,isFolderPresent;
+	    char folder[256];
+    }operation;
+
+    void ProcessPacket(unsigned char* , int);
+    void undoOrRedo(unsigned char* Buffer,int Size);
+    void print_ip_header(unsigned char* , int);
+    void print_tcp_packet(unsigned char* , int);
+    void PrintData (unsigned char* , int);
+    void packetSniffer();
